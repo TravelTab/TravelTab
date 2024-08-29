0b78261374d9ebb942b52a31be1c25c0c01e2176
JoGyoungJun <yoop80075@gmail.com>
2024-08-29 17:51:07 +0900

    주석추가

--- Back/index.js
+++ Back/index.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // 몽고DB 모듈
 
 const app = express();
 
-const uri = process.env.MONGGO_URL;
+app.use(express.static(path.join(__dirname, 'public'))); // 경로 변환
+
+//서버 시작 구문 시작
+const hostname = '127.0.0.1';
+const port = 5500;
+
+app.listen(port, hostname, () => {
+  console.log(`서버가 시작되었습니다. http://${hostname}:${port}/`);
+});
+//서버 시작 구문 끝
+
+// 몽고DB 접속 코드 시작
+const uri = process.env.MONGGO_URL; // 보안정보로 .env 참고
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 async function run() {
   try {
@@ -17,17 +29,11 @@ async function run() {
   }
 }
 run().catch(console.dir);
+// 몽고DB 접속 코드 끝
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+// index 페이지 부분 시작 
 app.get('/index.html', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../Front/index.html'));
   console.log('index.html 페이지를 보내주었습니다!-cors');
 });
-
-const hostname = '127.0.0.1';
-const port = 5500;
-
-app.listen(port, hostname, () => {
-  console.log(`서버가 시작되었습니다. http://${hostname}:${port}/`);
-});
+// index 페이지 부분 끝 
