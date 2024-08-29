ce494bf052ac9438cf11f0ed42eb215b34b9d7c4
JoGyoungJun <yoop80075@gmail.com>
2024-08-29 17:00:12 +0900

    몽고 DB 연동
    주가 npm은 package.json 확인

--- Back/index.js
+++ Back/index.js
@@ -1,7 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const uri = process.env.MONGGO_URL;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+async function run() {
+  try {
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(" MongoDB에 성공적으로 연결하였습니다.!");
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
