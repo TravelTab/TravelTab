0ccb00a6697f4c81e3d3d6f7374e27e0366f2285
Cinnamon <ryh1517@naver.com>
2024-08-31 21:22:21 +0900

    라우트 추가

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,61 +1,45 @@
-import './App.css';
-import Detail from './pages/Detail/Detail.js';
-import ExchangeRate from './pages/ExchangeRate/ExchangeRate.js';
-import FindingATM from './pages/FindingATM/FindingATM.js';
-import FirstPage from './pages/FirstPage/FirstPage.js';
-import Main from './pages/Main/Main.js';
-import MyCard from './pages/MyCard/MyCard.js';
-import MyTrip from './pages/MyTrip/MyTrip.js';
-import Notice from './pages/Notice/Notice.js';
-import Profile from './pages/Profile/Profile.js';
-import ProfileEdit from './pages/ProfileEdit/ProfileEdit.js';
-import SignIn from './pages/SignIn/SignIn.js';
-import TermOfUse from './pages/TermOfUse/TermOfUse.js';
-import TermOfUseClick from './pages/TermOfUseClick/TermOfUseClick.js';
-
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Detail from "./pages/Detail/Detail.js";
+import ExchangeRate from "./pages/ExchangeRate/ExchangeRate.js";
+import FindingATM from "./pages/FindingATM/FindingATM.js";
+import FirstPage from "./pages/FirstPage/FirstPage.js";
+import Main from "./pages/Main/Main.js";
+import MyCard from "./pages/MyCard/MyCard.js";
+import MyTrip from "./pages/MyTrip/MyTrip.js";
+import Notice from "./pages/Notice/Notice.js";
+import Profile from "./pages/Profile/Profile.js";
+import ProfileEdit from "./pages/ProfileEdit/ProfileEdit.js";
+import SignIn from "./pages/SignIn/SignIn.js";
+import TermOfUse from "./pages/TermOfUse/TermOfUse.js";
+import TermOfUseClick from "./pages/TermOfUseClick/TermOfUseClick.js";
+import Error from "./pages/Error.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Detail</h1>
-      <Detail/>
-
-      <h1>ExchangeRate</h1>
-      <ExchangeRate/>
-
-      <h1>FindingATM</h1>
-      <FindingATM/>
-      
-      <h1>FirstPage</h1>
-      <FirstPage/>
-
-      <h1>Main</h1>
-      <Main/>
-
-      <h1>MyCard</h1>
-      <MyCard/>
-
-      <h1>MyTrip</h1>
-      <MyTrip/>
-
-      <h1>Notice</h1>
-      <Notice/>
-
-      <h1>Profile</h1>
-      <Profile/>
-
-      <h1>ProfileEdit</h1>
-      <ProfileEdit/>
-
-      <h1>SignIn</h1>
-      <SignIn/>
-
-      <h1>TermOfUse</h1>
-      <TermOfUse/>
-
-      <h1>TermOfUseClick</h1>
-      <TermOfUseClick/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FirstPage />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/exchangerate" element={<ExchangeRate />} />
+        <Route path="/findingatm" element={<FindingATM />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/myCard" element={<MyCard />} />
+        <Route path="/mytrip" element={<MyTrip />} />
+        <Route path="/notice" element={<Notice />} />
+        <Route path="/profileedit" element={<ProfileEdit />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/termofuse" element={<TermOfUse />} />
+        <Route path="/termofuseclick" element={<TermOfUseClick />} />
+        <Route path="/error" element={<Error />} exact />
+        <Route path="/*" element={<Navigate replace to="/error" />} />
+      </Routes>
+    </Router>
   );
 }
 
