8a00da5c1d1b4a1ddcfc9fd485b750518053ec16
Taeyang1999 <ty5710@naver.com>
2024-09-05 21:41:34 +0900

    수정

--- Front/src/pages/Profile/Profile.js
+++ Front/src/pages/Profile/Profile.js
@@ -8,11 +8,22 @@ import '../Profile/components/Profile.css';
 
 const Profile = () => {
 
+const profile = [
+  {
+    name : "김토",
+    phone : "112",
+    email : "asd@naver.com",
+    address : "한경 아카데미",
+    card : "하나은행 트래블로그",
+    imgURL : "https://i.namu.wiki/i/8hbxBG7E76wK4L7fK3-B00boYbnvXR__jemXaT_Az1thuYsNqSrIgZ4CVh4kwo7bP3kYd2PSMVlJ9MdydkAeOoeyLQrOJXSDxjGiSdWJQE4_q25mzBg9N_hFB9_LdAOQLCQ-NFNzzas3TYEKOyYfVw.webp"
+  }
+]
+
   return (
     <DisplaySetting>
     <Header />
       <div className="container">
-        <Profiles/>
+        <Profiles myprofile={profile}/>
         <ProfileImage/>
         <ProfileInfo/>
         </div>
